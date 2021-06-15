7e318674bd68c2df0cad09099b174b78a1c76e70
Perrin Davidson <perrindavidson@Perrins-MacBook-Pro.local>
2021-06-15 13:32:04 -0500

    adding requirements.txt file

---
+++ data_prep/read_data/readData.py
@@ -0,0 +1,10 @@
+# thorium dynamics in the global ocean
+# ------------------------------------
+#            reading data
+# ------------------------------------
+#    author :: perrin w. davidson
+#    email  :: pwd@uchicago.edu
+# ------------------------------------
+#   load packages
+
+
